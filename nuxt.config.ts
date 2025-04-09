0e6e105c95f9982c4abeffe9be87e05bba7664fb
Filip Skoczeń <philipskoczen@gmail.com>
2025-04-09 10:48:56 +0200

    Init

---
+++ nuxt.config.ts
@@ -0,0 +1,20 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+
+  modules: [
+    '@nuxt/ui',
+    '@nuxt/eslint',
+    '@nuxt/fonts',
+    '@nuxt/icon',
+    '@nuxt/image'
+  ],
+
+  css: ['~/assets/css/main.css'],
+
+  future: {
+    compatibilityVersion: 4
+  },
+
+  compatibilityDate: '2024-11-27'
+})
